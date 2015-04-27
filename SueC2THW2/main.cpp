5b37f8b4bd6de4f967c5d913df27077ed8cb9b01
Chris Desch <cdesch@gmail.com>
2015-04-27 10:48:33 -0400

    Initial Commit

---
+++ SueC2THW2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SueC2THW2
+//
+//  Created by cj on 4/27/15.
+//  Copyright (c) 2015 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
